46df4d68cdf495feb30528b51d7273c1deaabda3
Lhoredel <lhoredeldollentas@gmail.com>
2025-10-03 14:40:35 +0800

    Happy Ladybugs.cs

---
+++ Happy Ladybugs.cs
@@ -0,0 +1,86 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'happyLadybugs' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING b as parameter.
+     */
+
+    public static string happyLadybugs(string b)
+    {
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+        foreach (char c in b)
+        {
+            if (freq.ContainsKey(c))
+                freq[c]++;
+            else
+                freq[c] = 1;
+        }
+
+        
+        foreach (var pair in freq)
+        {
+            if (pair.Key != '_' && pair.Value == 1)
+                return "NO";
+        }
+
+        if (!freq.ContainsKey('_'))
+        {
+    
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (i > 0 && i < b.Length - 1 && b[i] != b[i - 1] && b[i] != b[i + 1])
+                    return "NO";
+                if (i == 0 && b.Length > 1 && b[i] != b[i + 1])
+                    return "NO";
+                if (i == b.Length - 1 && b.Length > 1 && b[i] != b[i - 1])
+                    return "NO";
+            }
+        }
+
+        return "YES";
+    }
+
+    }
+
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int g = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int gItr = 0; gItr < g; gItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            string b = Console.ReadLine();
+
+            string result = Result.happyLadybugs(b);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
